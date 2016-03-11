6c5ed7c6aa301d19ea2883949a6afdc0cc25be00
YilingLeeDnd <yll615@ms31.hinet.net>
2016-03-11 21:13:05 +0800

    Added files via upload

---
+++ Homework1/[ CS263 ] 2016-03-08_Homework1/Problem1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte number0 = 254;
+            byte number1 = (byte)(number0 + 1);
+            //原number0 + 1程式判定為int(32位元)，所以把number0 + 1宣告成byte(8位元)
+            Console.WriteLine(number0);
+            Console.WriteLine(number1);
+        }
+    }
+}
---
+++ Homework1/[ CS263 ] 2016-03-08_Homework1/Problem2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student someone =
+                new Student
+                {
+                    name = "小李",
+                    Contact = new Contact
+                    {
+                        email = "lee@gmail.com",
+                        phone = "0910123456"
+                    }
+                };
+            Console.WriteLine(someone.name);
+            Console.WriteLine(someone.Contact.email);
+            Console.WriteLine(someone.Contact.phone);
+        }
+    }
+}
